9a054bce7142f8587f8c5d582b924323f0d3e134
CharLuney <charleen.beheray@ynov.com>
2024-12-06 10:00:36 +0100

    Start API

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	//"errors"
+)
 
+type Artist struct {
+	ID   string `json:id`
+	Name string `json:"name"`
+}
+
+var Artists = []Artist{
+	{ID: "1", Name: "Queen"},
+	{ID: "2", Name: "SOJA"},
+	{ID: "3", Name: "Pink Floyd"},
+}
+
+func Search(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Artists)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/Artists", Search)
+	router.Run("localhost:8080")
 }
