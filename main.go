f3eaca7ad2946e742591248367639d3f0cdb3a48
CharLuney <charleen.beheray@ynov.com>
2024-12-06 11:53:11 +0100

    API Structures

--- main.go
+++ main.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
 	//"errors"
 )
 
-type Artist struct {
-	ID   string `json:id`
-	Name string `json:"name"`
-}
-
-var Artists = []Artist{
-	{ID: "1", Name: "Queen"},
-	{ID: "2", Name: "SOJA"},
-	{ID: "3", Name: "Pink Floyd"},
-}
-
 func Search(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, Artists)
+	//c.IndentedJSON(http.StatusOK, artists)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/Artists", Search)
+	router.GET("/artists", Search)
 	router.Run("localhost:8080")
 }
--- scripts/struct.go
+++ scripts/struct.go
@@ -1,12 +1,36 @@
 package api
 
 var web Website
-var data Data
+var data []Data
 
 type Website struct {
 	Template string
 }
 
 type Data struct {
-	
+	// artists      []Artist
+	// locations    []Location
+	// concertdates []ConcertDate
+	// relation     []Relation
+}
+
+type Artist struct {
+	ID           string   `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationdate"`
+	FirstAlbum   string   `json:"firstalbum"`
+}
+
+type Location struct {
+	Locations []string
+}
+
+type ConcertDate struct {
+	ConcertDates []string
+}
+
+type Relation struct {
+	Relations []string
 }
