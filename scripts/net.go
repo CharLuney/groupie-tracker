693346c3c2348e79e99a60574381976fcd185434
enzo <enzo.granana@ynov.com>
2025-01-17 11:24:28 +0100

    Reapply "Merge branch 'enzo' into char"
    This reverts commit 6da6e91ed3487772e5203fed51125e3b275a3eb8.

--- scripts/net.go
+++ scripts/net.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 // Create the web server and waits for incoming user request
 func CreateWebsite() {
 	fmt.Println("Creating website..", web.Port)
-	http.HandleFunc("/", Index)
-	http.HandleFunc("/inspect", Inspect)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
+	fmt.Println("Assigning routes..")
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/filter", Filter)
+	http.HandleFunc("/inspect", Inspect)
 	http.ListenAndServe(web.Port, nil)
 }
 
@@ -20,9 +23,17 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	ParseTemplate(w)
 }
 
+func Filter(w http.ResponseWriter, r *http.Request) {
+	filters.MembersAmount = r.FormValue("Members")
+	fmt.Println(filters.MembersAmount)
+	ApplyFilters()
+	FilterTemplate(w)
+}
+
 func Inspect(w http.ResponseWriter, r *http.Request) {
 	web.Template = "web/inspect.html"
-	ParseTemplate(w)
+	id, _ := strconv.Atoi(r.FormValue("id"))
+	InspectTemplate(w, id)
 }
 
 // Parses HTML Template
@@ -38,6 +49,44 @@ func ParseTemplate(w http.ResponseWriter) {
 	}
 
 	err = tmpl.Execute(w, artists)
+	if err != nil {
+		fmt.Println("Error executing template:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+// Parses HTML Template for Filters
+func FilterTemplate(w http.ResponseWriter) {
+	fmt.Println("Parsing template.. ", web.Template)
+	tmpl, err := template.ParseFiles(web.Template)
+
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		fmt.Println("Template path: ", web.Template)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, artistsFilterted)
+	if err != nil {
+		fmt.Println("Error executing template:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+// Parses HTML Template for Inspect
+func InspectTemplate(w http.ResponseWriter, id int) {
+	fmt.Println("Parsing template.. ", web.Template)
+	tmpl, err := template.ParseFiles(web.Template)
+
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		fmt.Println("Template path: ", web.Template)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, artists[id])
 	if err != nil {
 		fmt.Println("Error executing template:", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
